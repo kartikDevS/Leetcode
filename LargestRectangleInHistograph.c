45112fb088b74d8466920304cc141bd006b3e041
kartikDevS <kartik9136@gmail.com>
2026-06-23 23:27:07 +0530

    leetcode 84

---
+++ LargestRectangleInHistograph.c
@@ -0,0 +1,19 @@
+int largestRectangleArea(int* heights, int heightsSize) {
+    int arr[heightsSize];int top=-1;
+    int Area=heights[0];int area;
+    arr[++top]=0;
+    int b,h;
+    for(int i=1;i<=heightsSize;i++){
+        int currentHeight = (i == heightsSize) ? 0 : heights[i];
+        while(top!=-1 && heights[arr[top]]>currentHeight){
+            h=heights[arr[top--]];
+            b=(top==-1)?i:(i-arr[top]-1);
+            area=h*b;
+            Area=(area>Area)?area:Area;
+        }
+        if(i<heightsSize){
+            arr[++top]=i;
+        }
+    }
+    return Area;
+}
