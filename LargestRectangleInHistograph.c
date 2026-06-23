63bd1436b7514d0ae4c4967aaf854c335c7319e3
kartikDevS <kartik9136@gmail.com>
2026-06-23 23:41:43 +0530

    no actually it was correct earlier only we dont require the brackets the confusion was that u were thinking arr[top] will be the index of the height u have(popped) but it will be the value after doing top-- i.e arr[top-1]

--- LargestRectangleInHistograph.c
+++ LargestRectangleInHistograph.c
@@ -7,7 +7,7 @@ int largestRectangleArea(int* heights, int heightsSize) {
         int currentHeight = (i == heightsSize) ? 0 : heights[i];
         while(top!=-1 && heights[arr[top]]>currentHeight){
             h=heights[arr[top--]];
-            b=(top==-1)?i:(i-(arr[top]-1));
+            b=(top==-1)?i:(i-arr[top]-1); 
             area=h*b;
             Area=(area>Area)?area:Area;
         }
